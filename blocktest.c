65e36deecff7f16d4289214c34879d60131edf92
JohnEfth <eft.giannis@gmail.com>
2017-02-17 20:52:17 +0200

    Block Implementation

--- blocktest.c
+++ blocktest.c
@@ -3,6 +3,12 @@
 
 int main(int argc,char **argv){
 
+int32_t k;
+
+printf("Size of int: %lu , size of 32int: %lu , pointer: %lu \n",sizeof(int),sizeof(k),sizeof(int32_t*));
+
+
+
 printf("This is the start of a new page , Karibu :D \n");
 
 return 0;
--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "block.h"
-
-int main(int argc,char **argv){
-
-printf("Lalalalala \n");
-
-return 0;
-}
