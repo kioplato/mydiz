90dcd112bfcf9ccc07904847e691e08c18c29f88
JohnEfth <eft.giannis@gmail.com>
2017-02-19 17:13:37 +0200

    Rename to file_structure

---
+++ file_structure.h
@@ -0,0 +1,39 @@
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+#define BLOCK_SIZE 512
+typedef struct DirName{
+	
+	char name[64];
+
+}DirName;
+
+typedef struct DiNode{
+
+	mode_t mode;    /* protection */
+	uid_t uid;		/* user ID of owner */                 
+	gid_t gid;		/* group ID of owner */
+	off_t size;		/* time of last status change */
+	time_t atime;        /* time of last access */
+    time_t mtime;        /* time of last modification */
+    time_t ctime;	    /* time of last status change */
+
+	char isDir;           /* 0 -> is File , 1 -> is Directory*/
+
+    uint32_t di_number[10];   /* Directory's DiNode */
+    DirName names[10];	  /* Directory's Name */
+
+    uint32_t next;      /*DiNode Number*/
+
+}DiNode;
+
+typedef struct Block{
+
+	char list_diNode;
+	
+
+}Block;
+
