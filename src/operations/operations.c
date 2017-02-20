a90ca87a369f127765cbd38c56ffa13c6912c36c
Plato Kiorpelidis <platwnace@gmail.com>
2017-02-20 17:33:07 +0200

    Added basic framework for the operations.

---
+++ src/operations/operations.c
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * File    : operations.c
+ * Purpose : Implementation for the functions that provide the operations.
+******************************************************************************/
+
+/* System Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool create_archive() {
+  
+  return true;
+}
+
+bool append_file() {
+  
+  return true;
+}
+
+bool extract_archive() {
+  
+  return true;
+}
+
+bool compress_file() {
+  
+  return true;
+}
+
+bool delete_entity() {
+  
+  return true;
+}
+
+bool print_metadata() {
+  
+  return true;
+}
+
+bool file_exists() {
+  
+  return true;
+}
+
+bool print_hierarchy() {
+  
+  return true;
+}
