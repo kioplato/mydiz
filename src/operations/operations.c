947fe089705e52bf762f80931e77e92576128e13
Plato Kiorpelidis <platwnace@gmail.com>
2017-02-21 00:13:55 +0200

    Partially implemented create_archive function.

--- src/operations/operations.c
+++ src/operations/operations.c
@@ -7,9 +7,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
+/********************/
 
-bool create_archive() {
+/* User Libraries */
+#include "operations.h"
+#include "../file_structure/file_structure.h"
+/******************/
+
+bool create_archive(const char* archive_name, const char** list_of_files, const int32_t numOf_files) {
+  int32_t fd = open(archive_name, O_WRONLY | O_CREAT, 0777);
+  Header* header = malloc(sizeof(Header));
+  write(fd, header, sizeof(Header));
   
+  //TODO: Iterate through the list_of_files.
+  //While we iterate we will write the contents of the
+  //files to the .di file and in parallel create and store
+  //a dinode in a list of dinodes so we can write the
+  //metadata afterwards.
+  
+  close(fd);
   return true;
 }
 
